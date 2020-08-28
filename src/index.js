fa51bfaf76b8ec1f0fd60d1563755009f62358a7
Ricqrdo <ricqrdo-@hotmail.com>
2020-08-28 14:30:40 -0500

    portfolio complete v1

---
+++ src/index.js
@@ -0,0 +1,26 @@
+require("./styles/main.scss");
+require("./images/code.jpg");
+require("./images/design.png");
+require("./images/face.jpg");
+require("./images/git.png");
+require("./images/javascript.svg");
+require("./images/react.svg");
+
+window.addEventListener("scroll", () => {
+  const asideLinks = document.querySelector(".aside-links");
+  const onScroll = document.documentElement.scrollTop;
+
+  if (window.innerWidth >= 1024) {
+    if (onScroll > 600) {
+      asideLinks.style.transform = "translate(0%, -50%)";
+      asideLinks.style.left = "5%";
+    } else {
+      asideLinks.style.left = "0%";
+      asideLinks.style.transform = "translate(-100%, -50%)";
+    }
+    if (onScroll > 2650) {
+      asideLinks.style.left = "0%";
+      asideLinks.style.transform = "translate(-100%, -50%)";
+    }
+  }
+});
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpeg|jpg|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[hash:6].[ext]",
+              outputPath: "images",
+            },
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "main.css",
+    }),
+  ],
+};
+
+// process.env.NODE_ENV !== "production"
+// ? "style-loader"
+// : MiniCssExtractPlugin.loader,
