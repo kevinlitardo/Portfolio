dea685c30f8f2198dc9d4f8075edd30169df5d56
kevin litardo <ricqrd-@hotmail.com>
2024-01-06 22:13:06 -0500

    Portfolio made with astro

---
+++ README.md
@@ -0,0 +1,5 @@
+# Welcome to my portfolio!
+
+Hi! Kevin here, this is my portfolio and you are able to check it out on [here!](www.ricqrdo.com)
+
+Well, there's not much left to say 😅
---
+++ src/helpers/Links.ts
@@ -0,0 +1,7 @@
+const LINKS = {
+  linkedin: "https://www.linkedin.com/in/kevinlitardo",
+  github: "https://github.com/kevinlitardo",
+  email: "k_litardo@outlook.com",
+};
+
+export default LINKS;
